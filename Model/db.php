56d5aa9d69b06e63a36c7ce1c463fb07917747a9
JulianDev1 <jmolinaruiz10@gmail.com>
2024-11-10 20:31:52 -0500

    ultimo guardado de todo

--- Model/db.php
+++ Model/db.php
@@ -1,10 +1,8 @@
 <?php
 class Database{
     public static function connect(){
-        $conexion = new mysqli("localhost", "root", "", "rdr_users");  
-
+        $conexion = new mysqli("localhost", "root", "root", "rdr_users");  
         $conexion -> query("SET NAMES 'utf8'");
          return $conexion;
-
     }
 } 
